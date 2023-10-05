b6bc97597a1626326086df1c1568da4dc1c0ea75
Rumil <cannirumil@yandex.ru>
2023-10-06 02:41:11 +0300

    add paginate for task

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
+use App\Utils\Paginator;
 use App\Service\TaskService;
 use App\Service\UserService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,36 +14,45 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class TaskController extends AbstractController
 {
+    private UserService $userService;
+    private TaskService $taskService;
+
+    public function __construct(UserService $userService, TaskService $taskService, ) {
+        $this->userService = $userService;
+        $this->taskService = $taskService;
+    }
 
     #[Route('/task', name: 'task')]
-    public function index(UserService $userService): Response
+    public function index(Request $request, Paginator $paginator, EntityManagerInterface $entityManager): Response
     {
-        $users = $userService->list();
+        $repository = $entityManager->getRepository(Task::class);
+        $query =  $repository->createQueryBuilder('t')->orderBy('t.id', 'DESC');
+        $paginator->paginate($query, $request->get('page', 1), 20);
+        $users = $this->userService->list();
         return $this->render('task/index.html.twig', [
             'users' => $users,
+            'paginator' => $paginator
         ]);
     }
 
     /**
-     * @param TaskService $taskService
      * @return Response
      */
     #[Route('/task/show-all', name: 'task_show_all', methods: ['GET'])]
-    public function showAll(TaskService $taskService): Response
+    public function showAll(): Response
     {
-        $tasks = $taskService->list();
+        $tasks = $this->taskService->list();
         return $this->json($tasks);
     }
 
     /**
      * @param int $id
-     * @param TaskService $taskService
      * @return Response
      */
     #[Route('/task/show/{id}', name: 'task_show', methods: ['POST'])]
-    public function show(int $id, TaskService $taskService): Response
+    public function show(int $id): Response
     {
-        $task = $taskService->get($id);
+        $task = $this->taskService->get($id);
         if (!$task) {
             return $this->json('Задача с id = ' . $id . ' не найдена!', 404);
         }
@@ -49,13 +61,12 @@ class TaskController extends AbstractController
 
     /**
      * @param int $id
-     * @param TaskService $taskService
      * @return Response
      */
     #[Route('/task/delete/{id}', name: 'task_delete', methods: ['DELETE'])]
-    public function delete(int $id, TaskService $taskService): Response
+    public function delete(int $id): Response
     {
-        $answer = $taskService->delete($id);
+        $answer = $this->taskService->delete($id);
         if (!$answer) {
             return $this->json('Ошибка при удалении задачи с id = ' . $id, 404);
         }
@@ -65,14 +76,13 @@ class TaskController extends AbstractController
     /**
      * @param Request $request
      * @param int $id
-     * @param TaskService $taskService
      * @return Response
      */
     #[Route('/task/update/{id}', name: 'task_update', methods: ['PUT'])]
-    public function update(Request $request, int $id, TaskService $taskService): Response
+    public function update(Request $request, int $id): Response
     {
         $data = $request->request;
-        $answer = $taskService->update($id, $data);
+        $answer = $this->taskService->update($id, $data);
         if (!$answer) {
             return $this->json('Ошибка при обновлении задачи с id = ' . $id, 404);
         }
@@ -81,14 +91,13 @@ class TaskController extends AbstractController
 
     /**
      * @param Request $request
-     * @param TaskService $taskService
      * @return Response
      */
     #[Route('/task/create', name: 'task_create', methods: ['POST'])]
-    public function create(Request $request, TaskService $taskService): Response
+    public function create(Request $request): Response
     {
         $data = $request->request;
-        $answer = $taskService->save($data);
+        $answer = $this->taskService->save($data);
         return $this->json(['success' => (bool)$answer]);
     }
 }
